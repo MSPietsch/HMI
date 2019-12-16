d4b8bdee9857aa7a1b48bcd460950a53248a67cb
MSPietsch <57191121+MSPietsch@users.noreply.github.com>
2019-12-16 15:01:47 +0100

    15:01 16.12.2019 Open Files, tkinter filedialog

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,11 @@
+import os, sys
+
+from tkinter.filedialog import askopenfilename
+
 from gui.UI import Ui_MainWindow
-import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+
 
 ##
 class MainWindow(QMainWindow):
@@ -9,19 +13,40 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.__connect_buttons()
 
-     #  self.__connect_buttons()
+    def __connect_buttons(self):
+        self.ui.btnNew.clicked.connect(self.newProject)
+        self.ui.btnOpen.clicked.connect(self.openProject)
+        self.ui.actionNew.triggered.connect(self.newProject)
+        self.ui.actionOpen.triggered.connect(self.openProject)
 
-   # def __connect_buttons(self):
-    #    self.ui.btn1.clicked.connect(self.fkt)
-     #   self.ui.actionTest.triggered.connect(lambda: print("TEST JA"))
-
-  #  def fkt(self):
-   #     self.ui.label1.setText("Hallo ich bin der coole Malte!")
+    #  def fkt(self):
+    #     self.ui.label1.setText("Hallo ich bin der coole Malte!")
     #    print("Test!")
 
-   # def malte(self):
-    #    print("Meine neue Fkt")
+    # TODO Funktionen für Buttons
+
+    def openProject(self):
+        name = askopenfilename(initialdir = "../Hazops",title = "RI - Fließbild wählen",filetypes = (("PDF Dateien","*.pdf"),("Alle Dateien","*.*")))
+        pass
+
+    def newProject(self):
+        try:
+            os.makedirs("../Hazops/Project_X")
+        except FileExistsError:
+            self.showPopup("folderExists")
+            pass
+
+    def showPopup(self, info):
+        if info == "folderExists":
+            msg = QMessageBox()
+            msg.setWindowTitle("Achtung!")
+            msg.setText("Dieser Ordner existiert bereits. Bitte wählen Sie einen anderen Projektnamen.")
+            msg.setIcon(QMessageBox.Warning)
+            msg.setStandardButtons(QMessageBox.Ok)
+            x = msg.exec_()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
