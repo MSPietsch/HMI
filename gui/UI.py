cbb4e18ff8774c8cba80039260c9b7c3552fd13e
MSPietsch <57191121+MSPietsch@users.noreply.github.com>
2019-12-16 12:21:23 +0100

    cool

---
+++ gui/UI.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '16_12_2019.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1082, 762)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 1081, 731))
+        font = QtGui.QFont()
+        font.setFamily("Myriad CAD")
+        font.setPointSize(14)
+        font.setUnderline(True)
+        self.frame.setFont(font)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.frame)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 1081, 731))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.labelUber = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.labelUber.setToolTip("")
+        self.labelUber.setWhatsThis("")
+        self.labelUber.setObjectName("labelUber")
+        self.gridLayout.addWidget(self.labelUber, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1082, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuDatei = QtWidgets.QMenu(self.menubar)
+        self.menuDatei.setObjectName("menuDatei")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionNew = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../icons/new_project_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionNew.setIcon(icon)
+        self.actionNew.setObjectName("actionNew")
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("../icons/Open_project_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionOpen.setIcon(icon1)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("../icons/save_project_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionSave.setIcon(icon2)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSchlie_en = QtWidgets.QAction(MainWindow)
+        self.actionSchlie_en.setObjectName("actionSchlie_en")
+        self.menuDatei.addAction(self.actionNew)
+        self.menuDatei.addAction(self.actionOpen)
+        self.menuDatei.addAction(self.actionSave)
+        self.menuDatei.addSeparator()
+        self.menuDatei.addAction(self.actionSchlie_en)
+        self.menubar.addAction(self.menuDatei.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.labelUber.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">HAZOP - TOOL</span></p></body></html>"))
+        self.menuDatei.setTitle(_translate("MainWindow", "Datei"))
+        self.actionNew.setText(_translate("MainWindow", "Neues Projekt"))
+        self.actionNew.setStatusTip(_translate("MainWindow", "Erstellt ein neues Projekt."))
+        self.actionNew.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        self.actionOpen.setText(_translate("MainWindow", "Projekt Öffnen"))
+        self.actionOpen.setStatusTip(_translate("MainWindow", "Öffnet ein Projekt."))
+        self.actionOpen.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.actionSave.setText(_translate("MainWindow", "Projekt Speichern"))
+        self.actionSave.setStatusTip(_translate("MainWindow", "Speichert das aktuelle Projekt."))
+        self.actionSave.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        self.actionSchlie_en.setText(_translate("MainWindow", "Schließen"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
