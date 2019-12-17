e00e3615a1f7ad1d597ffa2bc27d93ecf61a7ee6
MSPietsch <57191121+MSPietsch@users.noreply.github.com>
2019-12-17 14:26:15 +0100

    Bild Label im mainWidget

--- Code/gui/mainWidget.py
+++ Code/gui/mainWidget.py
@@ -13,19 +13,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_mainWidget(object):
     def setupUi(self, mainWidget):
         mainWidget.setObjectName("mainWidget")
-        mainWidget.resize(754, 384)
-        self.label = QtWidgets.QLabel(mainWidget)
-        self.label.setGeometry(QtCore.QRect(130, 140, 551, 16))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(mainWidget)
-        self.label_2.setGeometry(QtCore.QRect(250, 210, 55, 16))
-        self.label_2.setObjectName("label_2")
-        self.pushButton = QtWidgets.QPushButton(mainWidget)
-        self.pushButton.setGeometry(QtCore.QRect(270, 60, 93, 28))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../icons/HAZOP.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setObjectName("pushButton")
+        mainWidget.resize(979, 638)
+        self.RILabel = QtWidgets.QLabel(mainWidget)
+        self.RILabel.setGeometry(QtCore.QRect(4, 5, 971, 631))
+        self.RILabel.setText("")
+        self.RILabel.setPixmap(QtGui.QPixmap("../../../../Pictures/Handy/205153.jpg"))
+        self.RILabel.setObjectName("RILabel")
 
         self.retranslateUi(mainWidget)
         QtCore.QMetaObject.connectSlotsByName(mainWidget)
@@ -33,10 +26,6 @@ class Ui_mainWidget(object):
     def retranslateUi(self, mainWidget):
         _translate = QtCore.QCoreApplication.translate
         mainWidget.setWindowTitle(_translate("mainWidget", "Form"))
-        self.label.setText(_translate("mainWidget", "Neue Ansicht"))
-        self.label_2.setText(_translate("mainWidget", "Nik"))
-        self.pushButton.setStatusTip(_translate("mainWidget", "Hallo Nik"))
-        self.pushButton.setText(_translate("mainWidget", "Jo"))
 
 
 if __name__ == "__main__":
