0f7e228ee8ed9458d3cff02e7697228da3f75467
MSPietsch <57191121+MSPietsch@users.noreply.github.com>
2020-02-03 14:34:12 +0100

    Excel und wizards

--- Code/gui/nodeEdit.py
+++ Code/gui/nodeEdit.py
@@ -139,7 +139,7 @@ class Ui_nodeEdit(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\';\"><br /></p></body></html>"))
-        self.nodeNameEdit.setPlaceholderText(_translate("nodeEdit", "  0, 1, 2..."))
+        self.nodeNameEdit.setPlaceholderText(_translate("nodeEdit", " D1, B2..."))
         self.label_4.setText(_translate("nodeEdit", "Nebenknoten markieren"))
         self.nodeNameEdit_2.setHtml(_translate("nodeEdit", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
